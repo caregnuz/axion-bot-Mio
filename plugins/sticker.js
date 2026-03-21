451a97b109c4ff58095ccf832c8fd183ba64f4a8
bonzino <bonzinogoat@gmail.com>
2026-03-21 18:11:37 +0100

    update

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -7,13 +7,7 @@ let handler = async (m, { conn }) => {
 
     if (!/image|video|webp/.test(mime)) {
       return await conn.sendMessage(m.chat, {
-        text: '*『 ⚠️ 』- 𝐑𝐢𝐬𝐩𝐨𝐧𝐝𝐢 𝐚 𝐮𝐧’𝐢𝐦𝐦𝐚𝐠𝐢𝐧𝐞 𝐨 𝐚 𝐮𝐧 𝐯𝐢𝐝𝐞𝐨.*'
-      }, { quoted: m })
-    }
-
-    if (/video/.test(mime) && (q.msg || q).seconds > 10) {
-      return await conn.sendMessage(m.chat, {
-        text: '*『 ⚠️ 』- 𝐈𝐥 𝐯𝐢𝐝𝐞𝐨 𝐝𝐞𝐯𝐞 𝐝𝐮𝐫𝐚𝐫𝐞 𝐦𝐞𝐧𝐨 𝐝𝐢 𝟏𝟎 𝐬𝐞𝐜𝐨𝐧𝐝𝐢.*'
+        text: '*『 📱 』- 𝐑𝐢𝐬𝐩𝐨𝐧𝐝𝐢 𝐚 𝐮𝐧’𝐢𝐦𝐦𝐚𝐠𝐢𝐧𝐞 𝐨 𝐚 𝐮𝐧 𝐯𝐢𝐝𝐞𝐨.*'
       }, { quoted: m })
     }
 
@@ -23,14 +17,13 @@ let handler = async (m, { conn }) => {
     await conn.sendFile(m.chat, stiker, 'sticker.webp', null, m, true)
   } catch (e) {
     await conn.sendMessage(m.chat, {
-      text: '*『 ⚠️ 』- 𝐒𝐢 è 𝐯𝐞𝐫𝐢𝐟𝐢𝐜𝐚𝐭𝐨 𝐮𝐧 𝐞𝐫𝐫𝐨𝐫𝐞.*'
+      text: '*𝐄𝐫𝐫𝐨𝐫𝐞:* ' + e.message
     }, { quoted: m })
   }
 }
 
-handler.help = ['s']
+handler.help = ['s2']
 handler.tags = ['sticker']
-handler.command = ['s', 'sticker', 'stiker']
-handler.register = true
+handler.command = ['s','sticker']
 
 export default handler
