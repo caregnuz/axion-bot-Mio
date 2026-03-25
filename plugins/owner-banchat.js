a8e57c76c3bba24d186af69982aee7e88685f809
Jangl2004 <alessandromagno722@gmail.com>
2026-03-25 11:53:23 +0100

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,9 +1,10 @@
+
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = true
 m.reply('𝐌𝐨𝐝𝐚𝐥𝐢𝐭à 𝐀𝐅𝐊 𝐚𝐭𝐭𝐢𝐯𝐚𝐭𝐚 ✓')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
-handler.command = /^banchat$/i
+handler.command = /^off$/i
 handler.rowner = true
 export default handler
