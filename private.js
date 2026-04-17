0dde24379abce61aa54203581a716242ec2aeb70
Bonzino <falconemarco511@gmail.com>
2026-04-17 16:31:50 +0200

    edit private.js

--- private.js
+++ private.js
@@ -1,3 +1,3 @@
 global.botErrorChat = '120363424895913477@g.us';
 
-global.NuovoGruppoLink = https://chat.whatsapp.com/JELNl8JFvRbHkSl1173YFa
+global.NuovoGruppoLink = 'https://chat.whatsapp.com/JELNI8JFvRbHKSI1173YFa';
