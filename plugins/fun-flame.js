d2b0dedda2a604f325309889480421ad9ebe2bde
Bonzino <falconemarco511@gmail.com>
2026-04-21 20:45:13 +0200

    edit plugins/fun-flame.js

--- plugins/fun-flame.js
+++ plugins/fun-flame.js
@@ -255,7 +255,7 @@ handler.help = ['flame', 'stopflame']
 handler.tags = ['giochi']
 handler.command = /^(flame|stopflame)$/i
 handler.group = true
-handler.disable = true
+handler.disabled = true
 
 export default handler
 
